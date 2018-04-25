aa0a499bf5db0247010ab009910458b2ab7b2718
CodeDragon <hola@juliatrefas.com>
2018-04-25 18:47:57 +0200

    Update api_get_products.php

--- apis/api_get_products.php
+++ apis/api_get_products.php
@@ -1,7 +1,7 @@
 <?php
 
 // Load all the products and decode them to an array
-$sajProducts = file_get_contents('products.txt');
+$sajProducts = file_get_contents('../products.txt');
 $ajProducts = json_decode($sajProducts);
 
 // Encode then back to a string for display
